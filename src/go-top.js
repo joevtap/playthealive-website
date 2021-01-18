26b22b83b94fc2b2f226558c2402033c59d31c0f
Joel Vítor Torres <48035324+joevtap@users.noreply.github.com>
2021-01-18 11:22:51 -0300

    Add files via upload

---
+++ src/go-top.js
@@ -0,0 +1,13 @@
+//Get the button:
+button = document.getElementById("goTop");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
